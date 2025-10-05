9afa7eaec28aa72fbafe4300b39acdf45af8f786
petradeweese <petradeweese03@gmail.com>
2025-10-04 23:34:07 -0400

    Add favorites paper simulator scaffold

--- services/__init__.py
+++ services/__init__.py
@@ -1,5 +1,6 @@
 """Service layer package with shared configuration exports."""
 
 from config import settings as settings
+from . import favorites_sim as favorites_sim
 
-__all__ = ["settings"]
+__all__ = ["settings", "favorites_sim"]
