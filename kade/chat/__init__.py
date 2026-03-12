e2f2ce8fdcf5beedaed6f6b6e06c87b64a206f68
petradeweese <petradeweese03@gmail.com>
2026-03-12 10:58:05 -0400

    Revert "Add local Kade operator UI with deterministic chat + dashboard"

--- kade/chat/__init__.py
+++
@@ -1,4 +0,0 @@
-from .models import ChatReply, Intent
-from .service import ChatService
-
-__all__ = ["ChatReply", "Intent", "ChatService"]
--- kade/ui/__init__.py
+++
@@ -1,3 +0,0 @@
-from .app import create_ui_app
-
-__all__ = ["create_ui_app"]
